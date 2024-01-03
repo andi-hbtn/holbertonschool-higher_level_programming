4ac8072dcf2cf88a0825a07d855e7b189475c879
andi-bevapi <andi.bevapi@gmail.com>
2024-01-03 15:53:53 +0100

    changes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for x in range(0,10):
-        if i >= x:
-            continue
-        else:
+        if i < x:
             print('{}{}'.format(i, x), end=", ")
+        
