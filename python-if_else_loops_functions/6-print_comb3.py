770d4438851a2370b7ab7068640fa64be6fabe10
andi-bevapi <andi.bevapi@gmail.com>
2024-01-03 16:08:15 +0100

    changes

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(10):
     for x in range(0,10):
-        if i < x:
+        if i == 8 and x == 9:
+            print('{}{}'.format(i, x), end=" ")
+        elif i < x:
             print('{}{}'.format(i, x), end=", ")
         
