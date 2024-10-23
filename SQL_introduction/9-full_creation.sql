e0643800cd0ae333dd33c9de7272d6f0327dd5ab
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-10-23 12:06:24 +0200

    full-creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,6 @@
---script that create a table with specific attributes and check if it exist
+--script that create a table with specific attributes and check if it exist and insert values
 
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table(id int, name varchar(256),score int);
+
+INSERT INTO hbtn_0c_0.second_table (id,name,score)
+	VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
