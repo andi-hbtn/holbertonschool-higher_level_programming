a8f53508996fe19e188b47d6cb9263e1849e2251
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-10-30 10:59:42 +0000

    pycode style fixes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,19 +7,20 @@ connect with mysql and list all states
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
+if __name__ == 'main':
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
 
-# Example query
-try:
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-except MySQLdb.Error as e:
-    print(f"Error: {e}")
+    # Example query
+    try:
+        cursor.execute("SELECT * FROM states ORDER BY states.id")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
 
-# Closing the cursor and connection
-cursor.close()
-db.close()
+    # Closing the cursor and connection
+    cursor.close()
+    db.close()
