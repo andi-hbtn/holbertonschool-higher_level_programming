cae8fab621888ee4bb0855908b4816fa790b9415
andi-hbtn <andi.bevapi@holbertonschool.com>
2024-10-30 11:30:10 +0000

    listcitty starting by N letter

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+import MySQLdb
+import sys
+
+if __name__ == 'main':
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    # Example query
+    try:
+        query = (
+            "SELECT * FROM states "
+            "WHERE name LIKE 'N%' "
+            "ORDER BY states.id ASC"
+        )
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    # Closing the cursor and connection
+    cursor.close()
+    db.close()
