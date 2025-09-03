f8690a9ac6dddcce901472eade775f70c37d1e21
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-03 15:02:22 +0200

    changes

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -5,10 +5,13 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows;
 USE hbtn_0d_tvshows;
 -- Create the tv_shows table
 CREATE TABLE `tv_show_genres` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `show_id` int(11) NOT NULL,
+  `genre_id` int(11) NOT NULL,
+  KEY `show_id` (`show_id`),
+  KEY `genre_id` (`genre_id`),
+  CONSTRAINT `tv_show_genres_ibfk_1` FOREIGN KEY (`show_id`) REFERENCES `tv_shows` (`id`),
+  CONSTRAINT `tv_show_genres_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `tv_genres` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Create the tv_shows table
 CREATE TABLE `tv_shows` (
@@ -18,8 +21,8 @@ CREATE TABLE `tv_shows` (
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- Query to list all TV shows with their genre IDs
-SELECT tv_shows.title, tv_show_genres.id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.id
-ORDER BY tv_shows.title, tv_show_genres.id ASC;
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
