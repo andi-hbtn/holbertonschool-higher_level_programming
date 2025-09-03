6ccb48d5b160adb418b3075fe617200a1fbf9ef9
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-03 17:34:32 +0200

    0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,11 +1,11 @@
---write a script that print all privileges of user : user_0d_1
+-- write a script that print all privileges of user : user_0d_1
 -- create users 
 CREATE USER 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_2'@'localhost';
 
---grant users privileges
+-- grant users privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 SHOW GRANTS FOR 'user_0d_1' @ 'localhost';
---write a script that print all privileges of user : user_0d_2
+-- write a script that print all privileges of user : user_0d_2
 SHOW GRANTS FOR 'user_0d_2' @ 'localhost';
