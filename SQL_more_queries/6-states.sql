3cc6ad1b6b034f7d5f57735c6a38dbb03c16b356
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 14:19:31 +0200

    removed comments

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
---script to create a database and table states with values
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Use created database;
+
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states(id int UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,name VARCHAR(256) NOT NULL);
