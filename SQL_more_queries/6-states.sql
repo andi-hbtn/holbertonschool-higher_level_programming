bf4717cbf2b386663c6dece220a847118cdabd95
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 14:44:54 +0200

    changes

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,7 @@
---create database
+-- Create database and tables
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
---create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
