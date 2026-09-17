8d0c4115913050ba47dcbf0d183507112c79d4f9
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 13:40:23 +0200

    force_name table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+--script to create a database and table states with values
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL,name VARCHAR(256) NOT NULL);
