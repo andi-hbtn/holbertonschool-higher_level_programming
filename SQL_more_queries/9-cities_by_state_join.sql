4a379f656669cd186f81bfc5a0bbe32d8261bc88
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 16:32:18 +0200

    all cities

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 --list cities
 SELECT cities.id, cities.name, states.name
-FROM cities LEFT JOIN states
-ON cities.state_id  = states.id
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
