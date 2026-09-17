8c5d748c2b8829b1aaa3858d3e929e379e9eb572
Andi Bevapi <145327295+andi-hbtn@users.noreply.github.com>
2026-09-17 14:22:19 +0200

    Delete SQL_more_queries directory

--- SQL_more_queries/6-states.sql
+++
@@ -1,8 +0,0 @@
---create database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
---use database
-USE hbtn_0d_usa;
-
---create table
-CREATE TABLE IF NOT EXISTS states(id int UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,name VARCHAR(256) NOT NULL);
--- SQL_more_queries/README.md
+++
@@ -1,1 +0,0 @@
-# mysql_more_queries
