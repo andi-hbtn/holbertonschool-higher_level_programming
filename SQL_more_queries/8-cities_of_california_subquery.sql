f527e644a7843514dd8109d5f1423f0d340987e9
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 16:25:07 +0200

    all cities

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 --select all cities
-SELECT id,name
-FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = "California")
-ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
