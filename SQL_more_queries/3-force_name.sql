b284ca16501c5c8712231f8d750cf9b376ab5b87
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 12:20:22 +0200

    force_name table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
---force name table
-CREATE TABLE IF NOT EXISTS force_name(id INT,name VARCHAR(256) NOT NULL);
+--write a script that create a table force_name
+CREATE TABLE IF NOT EXISTS force_name (id int,name VARCHAR(256));
