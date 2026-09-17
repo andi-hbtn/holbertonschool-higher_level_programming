af66986afaefb74954edd4f4e773e9c4cdafd254
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 17:22:54 +0200

    unique_id table

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
 --Create unique_id table
-CREATE TABLE IF NOT EXISTS unique_id(id INT UNIQUE DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id( 
+    id INT UNIQUE DEFAULT 1,
+    name VARCHAR(256)
+);
