1842b8c6828f80d7bf762e07b725e2cfca8267db
andi-hbtn <andi.bevapi@holbertonschool.com>
2026-09-17 15:30:35 +0200

    changes

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL, 
-    FOREIGN KEY (state_id) REFERENCES ON cities(id) 
+    FOREIGN KEY (state_id) REFERENCES cities(id) 
 );
